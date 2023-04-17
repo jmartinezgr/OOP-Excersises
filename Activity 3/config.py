da2f764446e268d2e9fdfd33f156555e35e55e5b
Juan Jose Martinez <josemargri3@gmail.com>
2023-04-17 18:03:10 -0500

    Program constructor already config

--- Activity 3/config.py
+++ Activity 3/config.py
@@ -1,2 +1,12 @@
 from tkinter import *
 import os.path
+
+class Program:
+    def __init__(self,name,rezisable=False):
+        self.title = f'Ejercicio {name[0]} - Capitulo {name[1]}'
+        self.icono = './imagenes/descarga.ico'
+        self.alter_icon = './Activity 3/imagenes/descarga.ico'
+        self.size=  "770X470"
+        self.rezisable = rezisable
+
+
